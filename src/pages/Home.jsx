7345a0296606bb8b985ac7d372bc48009c8ba5fb
walisson27 <walisson_souza7@hotmail.com>
2023-05-24 15:54:07 -0300

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from "react"
+
+
+const movieURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home =()=>{
+  const [topMovies, setTopMovies] = useState([]);
+  
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data);
+  }
+
+  useEffect(() =>{
+
+  },[])
     return(
         <>
         </>
