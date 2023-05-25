dfa9145c238e0490cb58cd3f25653ec0809a6f3f
walisson27 <walisson_souza7@hotmail.com>
2023-05-25 16:57:38 -0300

    commit search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
+import './MovieGrid.css'
 
 const Search=()=>{
     return(
