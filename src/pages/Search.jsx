ea9cb96b301594aa8fd1e5f2d4207ff1927640ce
walisson27 <walisson_souza7@hotmail.com>
2023-05-25 17:05:07 -0300

    função search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,9 +8,20 @@ const apiKey = import.meta.env.VITE_API_KEY
 import './MovieGrid.css'
 
 const Search=()=>{
+    const [searhParams] = useSearchParams();
+    const [movies, setMovies] = useState([]);
+    const query = searhParams.get("q");
+
     return(
-        <>
-        </>
+        <div className="container">
+            <h2 className="title">Resultados para:
+            <span className="query-text">{query}</span>
+            </h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Carregando.....</p>}
+                {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+            </div>
+        </div>
     )
 }
 
