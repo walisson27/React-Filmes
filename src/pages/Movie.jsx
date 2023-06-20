dbd0c9a70b65af88a0a735656e4534926132754a
walisson27 <walisson_souza7@hotmail.com>
2023-06-19 23:31:28 -0300

    commit movie

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -7,11 +7,42 @@ import {
     BsFillFileEarmarkTextFill
 } from 'react-icons/bs'
 
+import MovieCard from "../components/MovieCard";
+import "./Movie.css"
+const movieURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie =()=>{
-    return(
-        <>
-        </>
+ const {id} = useParams()
+ const [movie, setMovie] = useState(null)
+
+ const getMovie = async(url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovie(data);
+  }
+
+  useEffect(() => {
+    const movieUrL = `${moviesURl}${id}?${apiKey}`;
+    getMovie(movieURL)
+  },[]);
+
+ }
+ return(
+    <div className="movie-page">
+            {movie && (
+                <>
+                <MovieCard movie={movie} showLink={false} />
+                <p className="tagline">{movie.tagline}</p>
+                <dir className="info">
+                    <h3>
+                        <BsWallet2/>
+                    </h3>
+                </dir>
+                </>
+            )}
+        </div>
     )
 }
-
 export default Movie;
